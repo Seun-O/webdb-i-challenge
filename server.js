e5b08af5d1ae0b26eda27b2f5dc08c29ebf180af
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-05-22 23:30:55 -0400

    MVP Complete working on adding comments

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
-const express = require('express');
-
+const express = require("express");
+const db = require("./data/accounts-model");
 const server = express();
 
 // your code here
+
+server.get("/api/accounts", async (req, res) => {
+  try {
+    const data = await db.find();
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error" });
+  }
+});
+
+/*
+    Route to fetch a specific ID an account /api/accounts/id
+    The change is stored in req.body and passed to the db.update handle along with the ID
+*/
+
+server.get("/api/accounts/:id", async (req, res) => {
+  try {
+    const data = await db.findById(req.params.id);
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error" });
+  }
+});
+
+/*
+    Route to update an account /api/accounts/id
+    The change is stored in req.body and passed to the db.update handle along with the ID
+*/
+server.put("/api/accounts/:id", async (req, res) => {
+  const change = req.body;
+  try {
+    const data = await db.update(req.params.id, change);
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ err, message: "Internal Server Error" });
+  }
+});
 
 module.exports = server;
